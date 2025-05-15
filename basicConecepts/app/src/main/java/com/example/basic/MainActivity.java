bcae11f06a485dc75399351535f8f21f9fbb47b1
parthmern <parthmern@gmail.com>
2025-05-14 20:43:00 -0400

    form

--- basicConecepts/app/src/main/java/com/example/basic/MainActivity.java
+++ basicConecepts/app/src/main/java/com/example/basic/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.basic;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Log.d("test", "this is testing msg");
+        button = findViewById(R.id.submit_button);
+        // u can also change color/text of this btn
+        button.setText("Submit Btn");
+
+        // attach event listener
+
+        View.OnClickListener viewListener = (view) ->{
+            // here code goes
+            Toast.makeText(this, "btn clicked", Toast.LENGTH_SHORT).show();
+        };
+
+        button.setOnClickListener((view) ->{
+            // here code goes
+            Toast.makeText(this, "btn clicked", Toast.LENGTH_SHORT).show();
+        });
     }
+
+
+
+
+
 }
